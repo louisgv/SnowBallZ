8437ddd7de62d87ef295798de79a348d8fa24d05
Pinktreeleaf <Pinktreeleaf@gmail.com>
2015-11-21 04:08:39 -0800

    Better positional movement and learned what makes OR and Unity happy

---
+++ Unity/Assets/ScalePositionScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScalePositionScript : MonoBehaviour {
+
+	public GameObject cameraToMove;
+	public Vector3 scale;
+	public Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+		startPosition = new Vector3 (cameraToMove.transform.position.x,
+		                            cameraToMove.transform.position.y,
+		                            cameraToMove.transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cameraToMove.transform.position = startPosition + new Vector3(
+			transform.localPosition.x * scale.x, 
+			transform.localPosition.y * scale.y,
+			transform.localPosition.z * scale.z);
+	}
+}
