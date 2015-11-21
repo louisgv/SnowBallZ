180caa0ccf16c09e6e9c1cc63e7a872eb97f03ea
Louis Graham Vichy <lgvichy@gmail.com>
2015-11-21 08:09:36 -0800

    Init HealthBar, NOTE: need some tweak on SOSO bot...

--- Unity/Assets/Components/Abstracts/HealthPoint.cs
+++ Unity/Assets/Components/Abstracts/HealthPoint.cs
@@ -1,11 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class HealthPoint : MonoBehaviour
 {
-
-	public int value = 9;
+	public Canvas canvas;
+	
+	public GameObject healthPrefab;
+	
+	public float healthPanelOffset = 0.35f;
+	public GameObject healthPanel;
+	private Slider healthSlider;
+	public int maxHealth = 9;
+	
+	private int health;
+	
+	void Start ()
+	{
+		health = maxHealth;
+	}
+	
+	void OnGUI ()
+	{
+		Vector2 targetPos;
 		
+		targetPos = Camera.main.WorldToScreenPoint (transform.position);
+		
+		GUI.Box (new Rect (targetPos.x, Screen.height - 20, 60, 20), health + "/" + maxHealth);
+		
+	}
+	
 	// Update is called once per frame
 	void Update ()
 	{
@@ -14,6 +37,6 @@ public class HealthPoint : MonoBehaviour
 	
 	public void OneDown ()
 	{
-		--value;
+		--health;
 	}
 }
